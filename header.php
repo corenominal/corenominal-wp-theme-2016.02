2782f11c4128d9400243a2a8e224f601bfef6de8
Philip Newborough <corenominal@corenominal.org>
2016-02-14 10:50:28 +0000

    Masthead

--- header.php
+++ header.php
@@ -4,10 +4,18 @@
  */
 require get_template_directory() . '/meta/default.php';
 ?>
-
+<header class="masthead">
+<div class="container">
+<h1 class="site-title"><a href="<?php bloginfo('url') ?>" title="<?php bloginfo('description') ?>"><?php bloginfo('name') ?></a></h1>
+<p class="site-description"><strong><?php bloginfo('description'); ?></strong></p>
+<nav>
 <?php
 /**
  * The header menu
  */
 wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
 ?>
+
+</nav>
+</div>
+</header>
