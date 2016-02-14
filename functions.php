725b7d762fd519caef13d7eedd7f457c8e795f90
Philip Newborough <corenominal@corenominal.org>
2016-02-14 10:30:35 +0000

    A header menu

--- functions.php
+++ functions.php
@@ -13,3 +13,8 @@ require get_template_directory() . '/functions/theme_support.php';
  * Enqueue scripts
  */
 require get_template_directory() . '/functions/enqueue_scripts.php';
+
+/**
+ * Menus
+ */
+require get_template_directory() . '/functions/menus.php';
--- header.php
+++ header.php
@@ -4,3 +4,10 @@
  */
 require get_template_directory() . '/meta/default.php';
 ?>
+
+<?php
+/**
+ * The header menu
+ */
+wp_nav_menu( array( 'theme_location' => 'header-menu' ) );
+?>
