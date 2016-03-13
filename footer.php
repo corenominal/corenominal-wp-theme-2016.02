8711130bdde84b1e0294f27d7f1829d1ccb150f9
Philip Newborough <corenominal@corenominal.org>
2016-03-13 19:03:18 +0000

    Structured data social links

--- footer.php
+++ footer.php
@@ -7,5 +7,19 @@
 <p class="footer-icons"><a href="<?php echo site_url( 'copyleft' ); ?>"><i class="fa fa-copyright fa-flip-horizontal"></i><span class="sr-only"> &copy; <?php echo date('Y') ?> Philip Newborough</span></a></p>
 </footer>
 <?php wp_footer() ?>
+<script type="application/ld+json">
+{ "@context" : "http://schema.org",
+  "@type" : "Organization",
+  "name" : "corenominal",
+  "url" : "https://corenominal.org",
+  "logo": "https://corenominal.orglogo.png",
+  "sameAs" : [ "http://www.facebook.com/corenominal",
+    "http://www.twitter.com/corenominal",
+    "https://plus.google.com/+PhilipNewborough",
+    "https://github.com/corenominal",
+    "https://codepen.io/corenominal",
+    "https://uk.linkedin.com/in/corenominal"]
+}
+</script>
 </body>
 </html>
